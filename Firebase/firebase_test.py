e085ddfc4a6b2c13966efeb5ba0424e5858866b9
Vincent Truong <vincenttruong.usa@gmail.com>
2025-11-20 21:45:06 -0500

    Implemented Firebase on Pi storage

--- Firebase/firebase_test.py
+++ Firebase/firebase_test.py
@@ -1,4 +1,9 @@
 import firebase_admin
 from firebase_admin import credentials, storage
 
-print("Firebase Admin SDK imported successfully!")
+print("Starting firebase test")
+
+cred = credentials.Certificate("/home/raspberrypi/yolo_project/WildLife-Detection/Firebase/real-time-wildlife-detector-firebase-adminsdk-fbsvc-7c1cbed963.json")
+firebase_admin.initialize_app(cred, {"storageBucket": "real-time-wildlife-detector.firebasestorage.app"})
+bucket = storage.bucket()
+print("Connected to Firebase Storage")
